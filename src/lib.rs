4b28b10d6f2b697706eebb553b39e91e45613576
jackxiao <465250521@qq.com>
2025-07-03 14:25:13 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+//! 这是一个示例库的文档
+
+/// 添加两个数字
+/// 
+/// # 示例
+/// ```
+/// use iot_driver_core::add;
+/// 
+/// assert_eq!(add(2, 3), 5);
+/// ```
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 3), 5);
+    }
+}
