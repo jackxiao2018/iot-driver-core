e2c349d6bfe9aae02ad15edccbdc79dacc73d86d
jackxiao <465250521@qq.com>
2025-07-23 15:12:17 +0800

    fixed bugs.

--- src/lib.rs
+++ src/lib.rs
@@ -85,3 +85,111 @@ pub struct DriverResult {
     pub attribute_actions: Vec<TslAttributeAction>,
 
 }
+
+/// 点位配置
+#[derive(Serialize, Deserialize)]
+pub struct DeviceTag {
+    // 设备标识符
+    #[serde(rename = "devId")]
+    pub device_id: String,
+    // 主键
+    pub id: String,
+    // 设备功能标签
+    pub identifier: String,
+    // 设备功能名称
+    pub identifier_name: String,
+    // 类型:事件,服务,属性
+    #[serde(rename = "type")]
+    pub tag_type: String,
+    // 采集间隔,单位ms
+    pub access_cycle: i64,
+    // 驱动功能名称
+    pub function_name: String,
+    // 驱动功能参数
+    pub function_param: String,
+    // 数据类型 INT DOUBLE FLOAT
+    pub data_type: String,
+    // 读写类型： R: 只读 W: 只写 RW: 读写
+    pub access_mode: String,
+}
+
+/// 网关驱动
+#[derive(Serialize, Deserialize)]
+pub struct GatewayDriver {
+
+    // 主键
+    pub id: String,
+    // 驱动id
+    #[serde(rename = "driverId")]
+    pub driver_id: String,
+    // 驱动文件名称
+    #[serde(rename = "fileName")]
+    pub file_name: String,
+    // 驱动存储路径
+    #[serde(rename = "filePath")]
+    pub file_path: String,
+    // 驱动包名
+    #[serde(rename = "packageName")]
+    pub package_name: String,
+    // 驱动类名
+    #[serde(rename = "clzName")]
+    pub class_name: String,
+
+}
+
+/// 驱动实例
+#[derive(Serialize, Deserialize)]
+pub struct GatewayDriverInstance {
+    // 主键
+    pub id: String, 
+    // 所属网关ID
+    #[serde(rename = "gatewayId")]
+    pub gateway_id: i64,
+    // 驱动实例名称 
+    pub name: String, 
+    // 驱动连接参数
+    #[serde(rename = "connectParams")]
+    pub connect_params: Vec<ConnectParam>,
+    // 关联的驱动
+    #[serde(rename = "gatewayDriver")]
+    pub gateway_driver: GatewayDriver,
+    // 设备列表
+    #[serde(rename = "deviceIds")]
+    pub device_ids: Vec<String>,
+    // 通道协议类型： 9、BACNetIP
+    #[serde(rename = "channelType")]
+    pub channel_type: String,
+}
+
+/// 设备基本信息
+#[derive(Serialize, Deserialize)]
+pub struct Device {
+    // 设备标识符
+    #[serde(rename = "devId")]
+    pub device_id: String,
+    // 设备编号
+    #[serde(rename = "devNo")]
+    pub device_number: String,
+    // 通讯方式：1.THIRD_PARTY_CONNECT:第三方接口方式 ; 2.DRIVER_CONNECT:驱动方式
+    #[serde(rename = "communicationMethod")]
+    pub communication_method: String,
+    // 产品key
+    #[serde(rename = "productKey")]
+    pub product_key: String,
+    // 设备名称
+    #[serde(rename = "devName")]
+    pub device_name: String,
+    // 设备secret
+    pub secret: String,
+    // 设备点表配置列表
+    #[serde(rename = "deviceTags")]
+    pub device_tags: Vec<DeviceTag>,
+    // 驱动实例
+    #[serde(rename = "gatewayDriverInstance")]
+    pub gateway_driver_instance: GatewayDriverInstance,
+    // 标签
+    pub extra: String,
+    // 租户ID
+    #[serde(rename = "tenantId")]
+    pub tenant_id: i32,
+}
